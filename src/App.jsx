2d0fb9d574e442c3242964f28bcee323d8dfb758
Abhijeet Patade <pattadeabhijeet@gmail.com>
2025-07-02 12:23:51 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
           show={showPopup}
           onClose={handleClosePopup}
           studentName="Abhijeet Dinkar Patade"
-          studentPhotoUrl="/public/images/name.jpg" // Path to their photo
+          studentPhotoUrl="/Images/name.jpg" // Path to their photo
           uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
         />
       </div>
